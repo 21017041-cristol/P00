72e69d168fa685c458746ee949b09ffd27f62f88
Crist <Crist@10.174.114.197>
2022-05-26 20:55:47 +0800

    first push to 100 P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,24 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * Crist, 26 May 2022 12:17:39 pm
+ */
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	public int subtract (int a, int b) {
+		return a - b;
+	}
+	public int multiple (int a, int b) {
+		return a * b; 
+	}
+	public int divide (int a, int b) {
+		return a / b;
+	}
+
+}
---
+++ P00/test/CalculatorTest2.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * Crist, 26 May 2022 2:01:33 pm
+ */
+
+public class CalculatorTest2 {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
